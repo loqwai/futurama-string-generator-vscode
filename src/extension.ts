e1cf3e057032b295e5bf6c52ddde4b85dad2beb6
redaphid <iam@aaronherres.com>
2021-09-05 01:23:20 -0700

    It works!!

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,12 @@
+/* eslint-disable @typescript-eslint/semi */
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-
+import randomFuturamaString from 'futurama-string-generator'
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
+
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "futurama-string-generator-vscode" is now active!');
@@ -16,11 +17,22 @@ export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('futurama-string-generator-vscode.generateFuturamaString', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from futurama-string-generator!');
+		const editor = vscode.window.activeTextEditor
+		if (!editor) {
+			return
+		}
+		const document = editor.document
+
+		editor.edit(editBuilder => {
+			editor.selections.forEach(sel => {				
+				const name = randomFuturamaString()
+				editBuilder.insert(sel.start, name);
+			})
+		})
 	});
 
 	context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
